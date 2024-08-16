4fba7efac5a4e61608d8c14d537282d90d8bd6c0
Savith Panamgama <geethikasavith@gmail.com>
2024-08-16 21:17:02 +0530

    fix: update attendance model

--- controllers/assistant/requestController.js
+++ controllers/assistant/requestController.js
@@ -1,13 +1,16 @@
-// import { getAttendanceModel } from '../../models/attendanceModel.js';
+import { getAttendanceModel } from '../../models/attendanceModel.js';
+import logger from '../../utils/logger.js';
 
 export const getAllnames = async (req, res) => {
   logger.info('Getting all names');
-  // try {
-  //   const Attendance = getAttendanceModel(req.dbConnection);
-  //   const names = await Attendance.find();
+  try {
+    const Attendance = getAttendanceModel(req.dbConnection);
+    const names = await Attendance.find();
 
-  //   logger.info(names);
-  // } catch (err) {}
-
-  res.status(200).json([]);
+    logger.info(names);
+    res.status(200).json(names);
+  } catch (err) {
+    logger.error('Error getting names:', err);
+    res.status(400).json({ error: err.message });
+  }
 };
